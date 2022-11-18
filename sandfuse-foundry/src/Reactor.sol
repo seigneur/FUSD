f0036e735f413e8787d62b82f1650aa448e47a2a
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2022-11-18 20:05:24 +0530

    Update Reactor.sol

--- sandfuse-foundry/src/Reactor.sol
+++ sandfuse-foundry/src/Reactor.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.16;
 
 import "openzeppelin-contracts/security/ReentrancyGuard.sol";
 import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import "v3-periphery/interfaces/ISwapRouter.sol";
+import "v3-periphery/libraries/TransferHelper.sol";
 import "./interfaces/IERC20Burnable.sol";
 import "./interfaces/IFusdNFT.sol";
 import "./Vault.sol";
@@ -17,7 +19,7 @@ contract Reactor is ReentrancyGuard {
     address immutable public collateral;
     address immutable public oracleAddress;
     address immutable public PoR;
-    address immutable public stakingContractAddress;
+    // address immutable public TREASURY;
     address immutable fusdERC20;
     uint256 constant durationForMaturity = 94608000;
     uint constant public decimals = 18;
@@ -25,20 +27,22 @@ contract Reactor is ReentrancyGuard {
     mapping(uint => int) public tokenIdPrices;
     uint public tokenLast;
 
+    ISwapRouter public immutable swapRouter;
+
     constructor(
         address _collateral, 
         address _fusdNFT, 
         address _oracle, 
         address _fusdERC20, 
         address _por,
-        address _stakingContractAddress
+        address _swapRouter
     ) {
         PoR = _por;
         collateral = _collateral;
         oracleAddress = _oracle;
         fusdNFT = IFusdNFT(address(_fusdNFT));
         fusdERC20 = _fusdERC20;
-        stakingContractAddress = _stakingContractAddress;
+        swapRouter = ISwapRouter(_swapRouter);
     }
 
     function borrow(uint256 amount)
@@ -48,9 +52,9 @@ contract Reactor is ReentrancyGuard {
         returns (uint256)
     {
         require(IVerifyPoR(PoR).verifyPoRCGT(), "Not enough reserves");
-        require(msg.value >= 0.0001 ether, "Insufficient fees to borrow!");
-        payable(address(stakingContractAddress)).transfer(0.00008 ether); // move to staking pool contract address to fund the lending side
-        block.coinbase.transfer(0.00002 ether);// pay the community, later change this to only if solo staker
+        require(msg.value >= 0.001 ether, "Insufficient fees to borrow!");
+        payable(address(this)).transfer(0.0008 ether); // move to staking pool contract address to fund the lending side
+        block.coinbase.transfer(0.0002 ether);// pay the community, later change this to only if solo staker
         uint256 fusePrice = uint256(
             priceFromOracle(oracleAddress)
         );
@@ -59,6 +63,7 @@ contract Reactor is ReentrancyGuard {
             75 * 
             uint256(fusePrice)) / (100 * (10**decimals * 10**decimals))) *
             (10**decimals);
+
         //move the funds to a vault to keep funds segregated
         address vaultCollateral = generateVault();
         IERC20(address(collateral)).safeTransferFrom(
@@ -136,6 +141,9 @@ contract Reactor is ReentrancyGuard {
                 IERC20(collateral),
                 sendToAddress
             );
+   
+            //transfer the FUSD amount from the borrower to this contract
+            IERC20Burnable(address(fusdERC20)).transferFrom(borrower, address(this), fusdAmt);
         } else {
             //assuming that the msg.sender/liquidator was funded by the treasury
             //TODO: Club into one txn to swap n uniswap
@@ -146,13 +154,25 @@ contract Reactor is ReentrancyGuard {
                 sendToAddress
             );
             //swap collateral
+            ISwapRouter.ExactInputSingleParams memory params =
+                ISwapRouter.ExactInputSingleParams({
+                    tokenIn: address(collateral),
+                    tokenOut: address(fusdERC20),
+                    fee: 3000,
+                    recipient: address(this),
+                    deadline: block.timestamp,
+                    amountIn: IERC20(collateral).balanceOf(address(this)),
+                    amountOutMinimum: 0,
+                    sqrtPriceLimitX96: 0
+                });
+            uint256 amountOut = swapRouter.exactInputSingle(params);
+            require(amountOut > 0, "swap failed");
         }
 
         IERC20Burnable(address(fusdERC20)).burnFrom(
             sendToAddress,
             fusdAmt
         );
-        console.log("Token URI - %s", fusdNFT.tokenURI(tokenId));
         fusdNFT.burn(tokenId);
         return true;
     }
